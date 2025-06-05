70952d0f41814a281a97f6d1dd7bfe766318bfc8
pushpabhagat <pushpa@werqlabs.com>
2025-06-05 15:38:47 +0530

    Update WeatherForecastCAController.cs 2

--- cicdapi/Controllers/WeatherForecastCAController.cs
+++ cicdapi/Controllers/WeatherForecastCAController.cs
@@ -21,6 +21,7 @@ namespace cicdapi.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
+        
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
