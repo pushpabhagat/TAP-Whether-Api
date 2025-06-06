749cf18e1ce6ed3397b444f11fa5cf565c10bb66
pushpabhagat <pushpa@werqlabs.com>
2025-06-06 16:53:29 +0530

    Update Program.cs

--- weatherapi/weatherapi/Program.cs
+++ weatherapi/weatherapi/Program.cs
@@ -5,6 +5,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 
+
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
