6d4a2a261ecb6868058e77d82224448ce73db6d4
pushpabhagat <pushpa@werqlabs.com>
2025-06-06 16:28:09 +0530

    Update WeatherForecast.cs

--- weatherapi/weatherapi/WeatherForecast.cs
+++ weatherapi/weatherapi/WeatherForecast.cs
@@ -3,7 +3,6 @@ namespace weatherapi
     public class WeatherForecast
     {
         public DateOnly Date { get; set; }
-
         public int TemperatureC { get; set; }
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
