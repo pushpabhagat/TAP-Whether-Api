489317b186899d5274397b242e7126dd66a98939
pushpabhagat <pushpa@werqlabs.com>
2025-06-06 17:53:46 +0530

    Update Program.cs

--- weatherapi/weatherapi/Program.cs
+++ weatherapi/weatherapi/Program.cs
@@ -7,6 +7,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 
+
 var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
